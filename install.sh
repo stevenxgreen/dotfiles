f081d35cb79dfb14fa4acc23481443ce53279579
stev <stevengreen@fastmail.com>
2022-03-20 17:38:48 +0000

    add zsh config

--- install.sh
+++ install.sh
@@ -7,13 +7,22 @@ mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
+#######
+# X11 #
+#######
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
 ######
 # i3 #
 ######
-
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+#######
+# zsh #
+#######
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
 
