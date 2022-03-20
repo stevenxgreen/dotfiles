d00c82001966fcb844571a24611aeba6aba34411
stev <stevengreen@fastmail.com>
2022-03-20 22:09:53 +0000

    more zsh

--- install.sh
+++ install.sh
@@ -26,4 +26,6 @@ mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
 
